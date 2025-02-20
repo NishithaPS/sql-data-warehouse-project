77fef4f216daa3d37c3f67b4ed47e012011cf813
NishithaPS <nishithasadashiva@gmail.com>
2025-02-20 16:09:04 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,13 @@
+use master
+GO
 
+-- Create databse 'DataWarehouse'
+create database DataWarehouse;
+GO
+  
+use DataWarehouse;
+
+-- Create Schemas
+create schema bronze;
+create schema silver;
+create schema gold;
